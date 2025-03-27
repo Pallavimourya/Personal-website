6ee01224fb3b0d2d861fd6f5043e8d0485398cd1
pallavi mourya <pallavimourya99@gmail.com>
2025-03-27 14:13:22 +0530

    edit link

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/navbar";
 import Intro from "./components/Intro/intro";
 import Skills from "./components/Skills/skills";
